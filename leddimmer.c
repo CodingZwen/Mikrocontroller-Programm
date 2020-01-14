704c49c83f05d8733f0c438ec419ab68b1de89f6
Sven <kuhumwerfer@hotmail.de>
2020-01-14 18:09:20 +0100

    Update leddimmer.c

--- leddimmer.c
+++ leddimmer.c
@@ -1,12 +1,4 @@
 
-/*
-"Erstes" C Programm von Sven-Patrick Schulze
-24.09.2019
-
-************************************************************
-*/
-
-
 #include <stdio.h>
 #include <c51rx2.h>
 
@@ -15,17 +7,6 @@
 unsigned char pwm_zaehler;
 unsigned char pwm_sollwert;
 
-
-//______
-
-
-/*
-sfr at P2 LEDs;
-sbit at P3_3 TA;
-sbit at P3_2 TZ;
-sbit at P1_7 SO; //schloss offen
-sbit at P1_6 SS; //schloss schlossen
-*/
 
 sbit at P3_2 Taster; //pwm eingang
 sbit at P2_0 led;
